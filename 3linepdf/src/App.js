c3d15f4ed0ed97f4cc4b4dcbf5c2d718d8bb5a67
KYHH <kyhh93@naver.com>
2023-08-15 18:58:30 +0900

    test

---
+++ 3linepdf/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="center">
+      <img src="/monkey.gif" alt="monkey" className="image"></img>
+    </div>
+  );
+}
+
+export default App;
